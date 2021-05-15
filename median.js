7be721d4227a0a24e6a227fec7e507ad69e283a5
hemantkumar2 <hemantnirmalkar17@gmail.com>
2021-05-15 19:07:55 +0530

    small correction

--- median.js
+++ median.js
@@ -1,17 +1,16 @@
 var findMedianSortedArrays = function (nums1, nums2) {
-  if (!nums1.length || !nums2.length) return 0;
-  const mergerdArr = nums1.concat(nums2).sort((a, b) => a - b);
-  if (mergerdArr.length === 1) return mergerdArr[0];
-  if (mergerdArr.length % 2 === 0) {
-    const median =
-      mergerdArr[mergerdArr.length / 2 - 1] + mergerdArr[mergerdArr.length / 2];
-    console.log(median);
-    return median / 2;
+  if (!nums1.length && !nums2.length) return 0;
+  const mergedArr = nums1.concat(nums2).sort((a, b) => a - b);
+  if (mergedArr.length === 1) return mergedArr[0];
+  if (mergedArr.length % 2 === 0) {
+    const sum =
+      mergedArr[mergedArr.length / 2 - 1] + mergedArr[mergedArr.length / 2];
+    return sum / 2;
   } else {
-    return mergerdArr[Math.ceil(mergerdArr.length / 2) - 1];
+    return mergedArr[Math.ceil(mergedArr.length / 2) - 1];
   }
 };
 
 const median = findMedianSortedArrays([1, 2], [3, 4]);
 
-// console.log(median);
+console.log(median);
