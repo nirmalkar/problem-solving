7d38bc3b5e7fd13014d3b20026b4fdac915edc4c
nirmalkar <hemantnirmalkar17@gmail.com>
2021-07-30 15:24:08 +0530

    changed server

---
+++ interview/Array.js
@@ -0,0 +1,13 @@
+//1.1 Given an array of integers, find the largest product yielded from three of the integers
+
+const unsortedArray = [-10, 7, 29, 30, 5, -10, -70];
+
+const computeProduct = () =>{
+    
+}
+
+
+computeProduct(unsortedArray); // 21000
+
+//Write your program here
+
