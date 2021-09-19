fd654698062c6a5bd597f687289c10a81b697695
nirmalkar <hemantnirmalkar17@gmail.com>
2021-09-19 21:26:24 +0530

    added prettier config

--- Array/ReturnUnique.js
+++ Array/ReturnUnique.js
@@ -1,18 +1,18 @@
 //  return all the unique numbers (ES6 Implementation)
-const array = [1, 2, 3, 5, 1, 5, 9, 1, 2, 8];
+const array = [1, 2, 3, 5, 1, 5, 9, 1, 2, 8]
 
 const returnUnique = (array) => {
-  // const set = new Set(array)
-  // console.log(set);
-  const numOccurrence = {};
-  array.forEach((element) => {
-    if (numOccurrence[element]) {
-      numOccurrence[element] = numOccurrence[element] + 1;
-    } else {
-      numOccurrence[element] = 1;
-    }
-  });
-  return Object.keys(numOccurrence);
-};
+    // const set = new Set(array)
+    // console.log(set);
+    const numOccurrence = {}
+    array.forEach((element) => {
+        if (numOccurrence[element]) {
+            numOccurrence[element] = numOccurrence[element] + 1
+        } else {
+            numOccurrence[element] = 1
+        }
+    })
+    return Object.keys(numOccurrence)
+}
 
-console.log(returnUnique(array));
+console.log(returnUnique(array))
--- String/isAnagram.js
+++ String/isAnagram.js
@@ -1,28 +1,28 @@
 // Given two strings, return true if they are anagrams of one another "Mary" is an anagram of "Army"
 
-var firstWord = "Mry";
-var secondWord = "Army";
+var firstWord = 'Mry'
+var secondWord = 'Army'
 
 const isAnagram = (s1, s2) => {
-  const strOne = s1.toLowerCase();
-  const strTwo = s2.toLowerCase();
-  const sameAlphabet = [];
-  if (strOne.length === strTwo.length) {
-    strOne.split("").forEach((ele, i) => {
-      if (strTwo.split("").includes(ele)) {
-        sameAlphabet.push(true);
-      } else {
-        sameAlphabet.push(false);
-      }
-    });
-  } else {
-    return `${s1} is not an anagram of ${s2}`;
-  }
-  if (!sameAlphabet.includes(false)) {
-    return `${s1} is an anagram of ${s2}`;
-  }
-  return `${s1} is not an anagram of ${s2}`;
-};
+    const strOne = s1.toLowerCase()
+    const strTwo = s2.toLowerCase()
+    const sameAlphabet = []
+    if (strOne.length === strTwo.length) {
+        strOne.split('').forEach((ele, i) => {
+            if (strTwo.split('').includes(ele)) {
+                sameAlphabet.push(true)
+            } else {
+                sameAlphabet.push(false)
+            }
+        })
+    } else {
+        return `${s1} is not an anagram of ${s2}`
+    }
+    if (!sameAlphabet.includes(false)) {
+        return `${s1} is an anagram of ${s2}`
+    }
+    return `${s1} is not an anagram of ${s2}`
+}
 
-const isStrAnagram = isAnagram(firstWord, secondWord);
-console.log(isStrAnagram);
+const isStrAnagram = isAnagram(firstWord, secondWord)
+console.log(isStrAnagram)
